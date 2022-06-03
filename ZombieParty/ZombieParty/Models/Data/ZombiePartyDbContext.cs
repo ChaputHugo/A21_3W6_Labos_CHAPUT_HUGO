6b1dec73858c2f607eb7ea1bca46ca627b07e7b3
Hugo Chaput <2064741@cegepmontpetit.ca>
2022-06-03 14:54:30 -0400

    CONF Configuration EntityFramwork
    Installation des packages minimum pour EntityFramework et SQL Server, création du ApplicationDbContext et ajout de DI du service SQL Server et de la connexion à la BD.

---
+++ ZombieParty/ZombieParty/Models/Data/ZombiePartyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZombieParty.Models.Data
+{
+    public class ZombiePartyDbContext
+    {
+    }
+}
