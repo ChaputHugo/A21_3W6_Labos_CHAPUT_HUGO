20e5d9f36a38958183593d55188dfb34efdd3c8a
Hugo CHaput <2064741@cegepmontpetit.ca>
2022-06-17 13:11:13 -0400

    CONFIG i18n
    Configuration de l’internationalisation

--- E22_3W6_Labos-S14_i18n_Models/ZombieParty/ZombieParty/Startup.cs
+++ E22_3W6_Labos-S14_i18n_Models/ZombieParty/ZombieParty/Startup.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using ZombieParty_DataAccess.Data;
@@ -17,6 +21,12 @@ namespace ZombieParty
 {
   public class Startup
   {
+    private CultureInfo[] supportedCultures = new[]
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("fr-CA")
+    };
+
     public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
@@ -27,9 +37,28 @@ namespace ZombieParty
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<ZombiePartyDbContext>(options =>
-options.UseSqlServer(
-  Configuration.GetConnectionString("DefaultConnection")));
+       #region Localization
+       services.AddLocalization(options => options.ResourcesPath = "Resources");
+       services.AddControllersWithViews().AddRazorRuntimeCompilation()
+               .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+               .AddDataAnnotationsLocalization();
+
+       /*.AddDataAnnotationsLocalization(options => {
+              options.DataAnnotationLocalizerProvider = (type, factory) =>
+                  factory.Create(typeof(SharedResource));
+              }); */
+
+       services.Configure<RequestLocalizationOptions>(options =>
+       {
+           options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+           options.SupportedCultures = supportedCultures;
+           options.SupportedUICultures = supportedCultures;
+       });
+       #endregion
+
+        services.AddDbContext<ZombiePartyDbContext>(options =>
+        options.UseSqlServer(
+        Configuration.GetConnectionString("DefaultConnection")));
 
       services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -39,6 +68,9 @@ options.UseSqlServer(
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+      app.UseRequestLocalization(locOptions.Value);
+
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
