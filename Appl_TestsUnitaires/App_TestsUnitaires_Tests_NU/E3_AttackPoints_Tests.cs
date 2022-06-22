77b24a3ab7557b41baacf379c8f568376ec60315
Hugo Chaput <2064741@cegepmontpetit.ca>
2022-06-22 11:26:51 -0400

    FCT Tests AttackPointsCalculator
    Tests AttackPointsCalculator, tous les scénarios et une exception

---
+++ Appl_TestsUnitaires/App_TestsUnitaires_Tests_NU/E3_AttackPoints_Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Appl_TestsUnitaires;
+using NUnit.Framework;
+
+namespace App_TestsUnitaires_Tests_NU
+{
+    [TestFixture]
+    public class E3_AttackPoints_Tests
+    {
+        E3_AttackPointsCalculator _zombieAttack;
+        [SetUp]
+        public void SetUp()
+        {
+            _zombieAttack = new E3_AttackPointsCalculator();
+        }
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(6, 0)]
+        [TestCase(19, 0)]
+        [TestCase(20, 0)]
+        [TestCase(21, 0)]
+        [TestCase(25, 1)]
+        [TestCase(30, 2)]
+        [TestCase(85, 13)]
+        public void AttackPointsCalculator_ReturnPoints(int nbrAttack, int expectedResult)
+        {
+            //Act
+            var result = _zombieAttack.CalculateAttackPoints(nbrAttack);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(205)]
+        public void AttackPointsCalculator_ThrowArgumentOutOfRangeException(int nbrAttack)
+        {
+            // Act et Assert
+            Assert.That(() => _zombieAttack.CalculateAttackPoints(nbrAttack), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+
+        }
+    }
+}
