e57a2b740d81b0846cb5fb310ef8f326f98bf5e6
Hugo Chaput <2064741@cegepmontpetit.ca>
2022-06-22 11:28:30 -0400

    FCT FizzBuzz Tests en XUnit
    Tests en XUnit

---
+++ Appl_TestsUnitaires/App_TestsUnitaires_Tests_XU/E1_FizzBuzz _Tests_V2.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace App_TestsUnitaires_Tests_XU
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
